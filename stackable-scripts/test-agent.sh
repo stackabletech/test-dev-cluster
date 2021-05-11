b71fb8783c12fdfa53544e820ac739818713ea81
Siegfried Weber <mail@siegfriedweber.net>
2021-05-11 16:22:16 +0200

    Test parameters forwarded to cargo test

--- stackable-scripts/test-agent.sh
+++ stackable-scripts/test-agent.sh
@@ -8,5 +8,5 @@ cd /agent-integration-tests
 
 # --test-treads=1 is required to prevent the suite from failing on the first run.
 # This is due to a bug in k3s.
-cargo test --target-dir /build/agent-integration-tests -- --nocapture --test-threads=1
+cargo test --target-dir /build/agent-integration-tests -- --nocapture --test-threads=1 $@
 
