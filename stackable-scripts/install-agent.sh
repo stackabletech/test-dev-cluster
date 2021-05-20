cafad702a34c8857d50c82feac08b0d58ef3f73f
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2021-05-20 18:23:37 +0200

    Only update the stackable repo when installing the agent.

--- stackable-scripts/install-agent.sh
+++ stackable-scripts/install-agent.sh
@@ -14,12 +14,14 @@ install_agent_package() {
     debian)
       apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 16dd12f5c7a6d76a
       echo deb https://repo.stackable.tech/repository/deb-release buster main | tee  /etc/apt/sources.list.d/stackable.list
-      apt-get update && apt-get install -y stackable-agent
+      apt-get update -o Dir::Etc::sourcelist="sources.list.d/stackable.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+      apt-get install -y stackable-agent
       ;;
     centos)
       # TODO: maybe add the gpg key here too
       yum-config-manager --add-repo=https://repo.stackable.tech/repository/rpm-release/el${VERSION}/
-      yum update && yum install -y stackable-agent --nogpgcheck
+      yum --disablerepo="*" --enablerepo="repo.stackable.*" update
+      yum install -y stackable-agent --nogpgcheck
       ;;
     *)
       echo [ERROR] Don\'t know how to install the stackable agent on this system.
